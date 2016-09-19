c2bcccede75e6225e65971a5abc6f7f238d44896
ulitosCoder <ulisesaj83@gmail.com>
2016-09-19 15:23:07 -0500

    Finished with observable array

--- js/app.js
+++ js/app.js
@@ -6,35 +6,39 @@ var initialCats=
             imgSrc : 'img/434164568_fea0ad4013_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
             showAdmin : false,
-            nicknames: [{nickname:'mittens'},{nickname:'fluffy'}]
+            nicknames: ['mittens','fluffy']
         },
         {
             clickCount : 0,
             name : 'Tiger',
             imgSrc : 'img/4154543904_6e2428c421_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
-            showAdmin : false
+            showAdmin : false,
+            nicknames: ['Tyger']
         },
         {
             clickCount : 0,
             name : 'Scaredy',
             imgSrc : 'img/22252709_010df3379e_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
-            showAdmin : false
+            showAdmin : false,
+            nicknames: null
         },
         {
             clickCount : 0,
             name : 'Shadowfalse',
             imgSrc : 'img/1413379559_412a540d29_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
-            showAdmin : false
+            showAdmin : false,
+            nicknames: null
         },
         {
             clickCount : 0,
             name : 'Sleepy',
             imgSrc : 'img/9648464288_2516b35537_z.jpg',
             imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
-            showAdmin : false
+            showAdmin : false,
+            nicknames: null
         }
     ];
 
@@ -76,11 +80,9 @@ Cat = function(data){
 
 ViewModel= function(){
     var self = this;
-
-    
-
-
     this.catList = ko.observableArray([]);
+    this.currentCat = null;
+
     initialCats.forEach(function(catItem){
         self.catList.push(new Cat(catItem));
     });
@@ -89,6 +91,11 @@ ViewModel= function(){
 
     this.incrementCounter = function(){
         self.currentCat().incrementCounter();
+    };
+
+    this.setCurrentCat = function(cat){
+        self.currentCat( cat );
+        //alert(self.currentCat().nicknames);
     };
 };
 

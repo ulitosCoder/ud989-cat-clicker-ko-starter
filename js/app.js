6d3129f88f4457b75e200e1048f5edd2b490a304
ulitosCoder <ulisesaj83@gmail.com>
2016-09-19 13:43:08 -0500

    Show list of cats with Knockout

--- js/app.js
+++ js/app.js
@@ -1,21 +1,54 @@
-var aModel= {
-
-};
+var initialCats= 
+    [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            showAdmin : false,
+            nicknames: [{nickname:'mittens'},{nickname:'fluffy'}]
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            showAdmin : false
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            showAdmin : false
+        },
+        {
+            clickCount : 0,
+            name : 'Shadowfalse',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            showAdmin : false
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            showAdmin : false
+        }
+    ];
 
 
 
 
 Cat = function(data){
+
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgSrc);
     
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Taby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
-    
-    this.nicknames = ko.observableArray(
-        [{nickname:'mittens'},
-        {nickname:'fluffy'}]
-        );
+    this.nicknames = ko.observableArray(data.nicknames);
 
     this.incrementCounter = function(){
         this.clickCount(this.clickCount() + 1 ) ;
@@ -44,7 +77,15 @@ Cat = function(data){
 ViewModel= function(){
     var self = this;
 
-    this.currentCat = ko.observable(new Cat());
+    
+
+
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function(){
         self.currentCat().incrementCounter();
